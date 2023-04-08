ad73a1789ce8b08a581f59963a91351a02e12c36
Aneesh <sunnykumar12928@gmail.com>
2023-04-08 13:22:31 +0530

    added particle background

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import MouseTrail from './components/shared/MouseTrail'
+import ParticleBackground from './components/shared/ParticleBackground'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <MouseTrail/>
   </React.StrictMode>,
 )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,20 @@ import Spline from "@splinetool/react-spline";
 import backgroundImage from "/background.jpg";
 import "./Home.css";
 import Countdown from "../../components/shared/Countdown";
+import ParticleBackground from "../../components/shared/ParticleBackground";
 
 const Home = () => {
   return (
     <div
       className="h-screen w-full m-0"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-        backgroundAttachment: "fixed",
-      }}
+      // style={{
+      //   backgroundImage: `url(https://wallpapercave.com/wp/wp8017879.jpg)`,
+      //   backgroundSize: "cover",
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundPosition: "center center",
+      // }}
     >
+      <ParticleBackground/>
       <div className="top-10 h-full flex justify-center items-center flex-col ">
         <div className="h-3/4 w-full">
           <Spline scene="https://prod.spline.design/vakXcAJXStGdHotW/scene.splinecode"  />
