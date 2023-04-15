6e16021f1f0223de4b69c05166e51bf39541b2c9
MridulTi <mridultiwari2002@gmail.com>
2023-04-15 11:09:43 +0530

    Changed orientation of Team page

--- src/components/shared/Teams/Teamscards.jsx
+++ src/components/shared/Teams/Teamscards.jsx
@@ -5,20 +5,20 @@ import { Container } from "./style"
 import {LazyLoadImage} from "react-lazy-load-image-component"
 
 
-export function Cardover(props){
-    const cardstyle={
-        backgroundColor:props.color
-    }
-    return(
-        <div className='grid grid-cols-2 cardover rounded-2xl xs:w-56 sm:w-56 md:w-56 xl:w-60' style={cardstyle}>
-            <div className='grid grid-cols-2 gap-12 z-10'>
-                <h1 className='text-4xl font-bold py-5 px-2'>{props.name}</h1>
-            <a href={props.link} target="_blank"><div className='flex gap-2 align-text-top'><IoLogoWhatsapp/>Whatsapp</div></a>
-            </div>
-            <LazyLoadImage src={props.image} alt="Image" />
-        </div>
-    )
-}
+// export function Cardover(props){
+//     const cardstyle={
+//         backgroundColor:props.color
+//     }
+//     return(
+//         <div className='grid grid-cols-2 cardover rounded-2xl xs:w-56 sm:w-56 md:w-56 xl:w-60' style={cardstyle}>
+//             <div className='grid grid-cols-2 gap-12 z-10'>
+//                 <h1 className='text-4xl font-bold py-5 px-2'>{props.name}</h1>
+//             <a href={props.link} target="_blank"><div className='flex gap-2 align-text-top'><IoLogoWhatsapp/>Whatsapp</div></a>
+//             </div>
+//             <LazyLoadImage src={props.image} alt="Image" />
+//         </div>
+//     )
+// }
 export function CardOverlay(props){
     const [hover, setHover] = React.useState(false);
     return(
@@ -34,18 +34,18 @@ export function CardOverlay(props){
       }}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      className="cardover rounded-full w-72 h-72 xs:w-52 xs:h-52 sm:w-52 sm:h-52 md:w-56 md:h-56 lg:w-52 lg:h-52 xl:w-60 xl:h-60 "
+      className="cardover rounded-full w-32 h-32 xs:w-36 xs:h-36 sm:w-36 sm:h-36 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-40 xl:h-40 2xl:w-44 2xl:h-44 "
     >
       <div
         style={{
           display: hover ? "flex" : "none",
           flexDirection: "column",
           alignItems: "center",
-          justifyContent:"center"
+          justifyContent:"center",
         }}
       >
-        <div className="font-bold text-xl mb-2">{props.name}</div>
-        <a href={props.link} className='px-2 flex gap-2 items-center justify-center'><IoLogoWhatsapp className="text-3xl text-emerald-400 hover:text-emerald-300"/>Whatsapp</a>
+        <div className="font-bold text-sm xs:text-sm sm:text-sm md:text-md lg:text-lg mb-2">{props.name}</div>
+        <a href={props.link} className='px-2 flex gap-2 items-center justify-center text-sm xs:text-xs sm:text-sm md:text-md lg:text-lg'><IoLogoWhatsapp className="text-2xl text-emerald-400 hover:text-emerald-300"/>Whatsapp</a>
       </div>
     </Container>
     )
