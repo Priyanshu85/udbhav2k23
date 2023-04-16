296a7df5869ee85f7b6f32c01b097964084f2373
Aneesh <sunnykumar12928@gmail.com>
2023-04-16 13:22:41 +0530

    in building component added and teams arranged

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,12 +9,12 @@ import Teaser from "../../components/Home/Teaser";
 const Home = () => {
   return (
     <div
-      className="min-h-screen lg:w-full m-0"
+      className="min-h-screen lg:w-full"
     >
-      <ParticleBackground />
+      
       <div className="top-10 h-full flex justify-center items-center flex-col ">
-        <div className="h-3/4 lg:w-full w-full">
-          <Spline scene="https://prod.spline.design/nah8ZYm1kfWoaNPW/scene.splinecode" />
+        <div className="h-[50vh] lg:h-[70vh] lg:w-full w-full">
+          <Spline scene="https://prod.spline.design/nah8ZYm1kfWoaNPW/scene.splinecode"/>
         </div>
         <Countdown />
         <Teaser />
