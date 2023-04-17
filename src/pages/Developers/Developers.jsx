c6df026d01ab1173db6fa43dc3f126bf2b3fc642
Priyanshu85 <pg7909290@gmail.com>
2023-04-18 00:51:46 +0530

    Added Developers Page

---
+++ src/pages/Developers/Developers.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Developers.scss'
+
+const Developers = () => {
+  return (
+    <div className="Team">
+    <header>
+      <div className="title">
+        <img className="TeamHeading" src={TeamHeading} alt="Team Zero" />
+      </div>
+      <div className="content">
+        <h5>who we are</h5>
+        <p>
+          We are team of creatively diverse. driven. innovative programmers.
+        </p>
+      </div>
+    </header>
+    <main>
+      <div className="profile">
+        <figure data-value="Frontend Developer">
+          <img src={require("../../assets/nishant.jpeg")} alt="" />
+          <figcaption>Nishant Sharma</figcaption>
+        </figure>
+      </div>
+      <div className="profile">
+        <figure data-value="Frontend Developer">
+          <img src={require("../../assets/Pranshu.jpg")} alt="" />
+          <figcaption>Pranshu Singh</figcaption>
+        </figure>
+      </div>
+      <div className="profile">
+        <figure data-value="Frontend Developer">
+          <img src={require("../../assets/Samridhi.jpg")} alt="" />
+          <figcaption>Samridhhi Yadav</figcaption>
+        </figure>
+      </div>
+      <div className="profile">
+        <figure data-value="Frontend Developer">
+          <img src={require("../../assets/Dhruv.jpg")} alt="" />
+          <figcaption>Dhruv Varshney</figcaption>
+        </figure>
+      </div>
+    </main>
+  </div>
+  )
+}
+
+export default Developers
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as About } from './About/About';
 export { default as Teams } from './Teams/Teams';
 export { default as Events } from './Events/Events';
 export { default as SingleEvent } from './Events/SingleEvent';
+export { default as Developers } from './Developers/Developers';
