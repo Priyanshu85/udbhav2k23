8bed2e37f3c4933ec48d281942c5b4fffab2f7a8
Priyanshu Gupta <83485475+Priyanshu85@users.noreply.github.com>
2023-04-18 13:00:21 +0530

    Update Header.jsx

--- src/components/shared/Navbar/Header.jsx
+++ src/components/shared/Navbar/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
             </div>
             <div className="max-lg:order-3 lg:w-4/12 text-end">
             <a href="https://udbhavipec.eventbrite.com" target="_blank" className="relative inline-flex items-center justify-center px-10 py-3 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group">
-<span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#D57E0A] rounded-full group-hover:w-56 group-hover:h-56"></span>
+<span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#D57E0A] rounded-full group-hover:w-full group-hover:h-56"></span>
 <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
 <span className="relative">Register for Entry Pass</span>
 </a>
