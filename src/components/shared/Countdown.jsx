a52d583a634d0064f8e66bf8de7a0c8340f36d68
Aneesh <sunnykumar12928@gmail.com>
2023-04-28 00:17:55 +0530

    fixed timer bug

--- src/components/shared/Countdown.jsx
+++ src/components/shared/Countdown.jsx
@@ -9,7 +9,7 @@ const getTimeLeft = () => {
   const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
   const seconds = Math.floor((totalTimeLeft / 1000) % 60);
-  return { days, hours, minutes, seconds };
+  return totalTimeLeft<0 ? {days:0, hours:0,minutes:0,seconds:0} : { days, hours, minutes, seconds };
 };
 
 const Countdown = () => {
